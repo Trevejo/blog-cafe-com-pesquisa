4094541dc02f7122828ccab81b85f5ca4e4acb45
Murilo Trevejo <murilotrevejo09@gmail.com>
2025-09-04 20:29:56 -0300

    fix: production path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://trevejo.github.io",
+  base: "/blog-cafe-com-pesquisa",
+  integrations: [mdx(), sitemap()],
 });
