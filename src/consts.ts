606125ab0009cbc2fbb879ab9e3d4da557e93c4a
Trevejo <murilotrevejo09@gmail.com>
2025-09-06 12:57:18 -0300

    feat: cozy interface and home screen

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Café Cinzento";
-export const SITE_DESCRIPTION = "Postagens feitas a base de café.";
+export const SITE_TITLE = "Café com Pesquisa";
+export const SITE_DESCRIPTION = "Postagens de pesquisas feitas a base de café.";
 export const BASE_PATH = import.meta.env.BASE_URL;
